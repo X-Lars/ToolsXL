7908396658af133b54447eb306c5f7b445582732
XLars <49834485+X-Lars@users.noreply.github.com>
2021-01-20 16:57:05 +0100

    Removed options outside

--- ToolsXL/Extensions/StringExtensions.cs
+++ ToolsXL/Extensions/StringExtensions.cs
@@ -1,7 +1,19 @@
-﻿using System.Text.RegularExpressions;
+﻿using System;
+using System.Text.RegularExpressions;
 
 namespace ToolsXL.Extensions
 {
+    /// <summary>
+    /// Defines the options to be used by the <see cref="Like"/> method.
+    /// </summary>
+    [Flags]
+    public enum LikeOptions
+    {
+        Default   = 0, // Case insensitive, respect begin and end
+        MatchCase = 1, // Case sensitive
+        Implicit  = 2  // Filter is prefixed and postfixed with a wildcard
+    }
+
     public static class StringExtensions
     {
         /// <summary>
@@ -12,15 +24,20 @@ namespace ToolsXL.Extensions
         /// <param name="prefixWildcard">Specifies if a wildcard prefix is added to the filter.</param>
         /// <returns>A <see cref="bool"/> containing true if the <paramref name="source"/> matches the <paramref name="filter"/> pattern.</returns>
         /// <remarks><i>Use ' ' or '*' as wildcard for any number of characters, use '?' as single character wildcard.</i></remarks>
-        public static bool Like(this string source, string filter, bool prefixWildcard = false)
+        public static bool Like(this string source, string filter, LikeOptions options = LikeOptions.Default)
         {
-            if (prefixWildcard == false)
+            string wildcard = string.Empty;
+
+            if (options.HasFlag(LikeOptions.Implicit))
+                wildcard = ".*";
+
+            if (options.HasFlag(LikeOptions.MatchCase))
             {
-                return new Regex(@"^.*" + Regex.Escape(filter).Replace(@"\?", ".").Replace(@"\ ", ".*").Replace(@"\*", ".*") + ".*$", RegexOptions.IgnoreCase | RegexOptions.Singleline).IsMatch(source);
+                return new Regex($@"^{wildcard}" + Regex.Escape(filter).Replace(@"\?", ".").Replace(@"\ ", ".*").Replace(@"\*", ".*") + $"{wildcard}$", RegexOptions.Singleline).IsMatch(source);
             }
             else
             {
-                return new Regex(@"^" + Regex.Escape(filter).Replace(@"\?", ".").Replace(@"\ ", ".*").Replace(@"\*", ".*") + ".*$", RegexOptions.IgnoreCase | RegexOptions.Singleline).IsMatch(source);
+                return new Regex($@"^{wildcard}" + Regex.Escape(filter).Replace(@"\?", ".").Replace(@"\ ", ".*").Replace(@"\*", ".*") + $"{wildcard}$", RegexOptions.IgnoreCase | RegexOptions.Singleline).IsMatch(source);
             }
         }
     }
